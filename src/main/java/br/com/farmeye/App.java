19b10a3337bfade13c0047cc094458a72f7062d2
luiz <luizaugusto3b@gmail.com>
2017-12-24 14:15:48 -0200

    Download from AWS sentinel finished

--- src/main/java/br/com/farmeye/App.java
+++ src/main/java/br/com/farmeye/App.java
@@ -1,5 +1,7 @@
 package br.com.farmeye;
 
+import com.google.common.base.Strings;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -7,8 +9,11 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxDriverLogLevel;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.*;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -17,11 +22,9 @@ import java.util.stream.Collectors;
 
 public class App {
 
-	//private static final String URL = "http://sentinel-s2-l1c.s3-website.eu-central-1.amazonaws.com/#tiles/21/L/UE/";
-	//private static final String URL = "http://sentinel-s2-l1c.s3-website.eu-central-1.amazonaws.com/#tiles/21/L/UE/2015/11/";
-	private static final String URL = "http://sentinel-s2-l1c.s3-website.eu-central-1.amazonaws.com/#tiles/21/L/UE/2015/11/17/";
 	private static final String PATH_WEBDRIVER = App.class.getClassLoader().getResource("geckodriver").getPath();
-	private static final ExecutorService EXECUTOR = Executors.newCachedThreadPool();
+	private static final Integer POOL_SIZE = Runtime.getRuntime().availableProcessors() + 1;
+	private static final ExecutorService EXECUTOR = Executors.newFixedThreadPool(POOL_SIZE);
 
 	private static List<String> nextBranch(String url) throws InterruptedException {
 
@@ -38,7 +41,7 @@ public class App {
 
 		boolean isEnd = !(breadcrumbs.findElements(By.tagName("a")).size() < 9);
 
-		List<String> listFilesToDownload = new ArrayList<>();
+		Set<String> listFilesToDownload = new HashSet<String>();
 
 		if(isEnd){
 			listFilesToDownload.addAll(getLeafs(driver));
@@ -49,7 +52,7 @@ public class App {
 
 		driver.quit();
 
-		return listFilesToDownload;
+		return new ArrayList<>(listFilesToDownload);
 	}
 
 	private static List<String> getLeafs(WebDriver driver) {
@@ -84,15 +87,71 @@ public class App {
 				});
 	}
 
+	private static void createSaveFile(File whereToSave, String url) {
+		String path = url.replaceFirst("^(http[s]?://www\\.|http[s]?://|www\\.)","");
+		List<String> pathArray = new ArrayList<>(Arrays.asList(path.split("/")));
+		pathArray.remove(0);
+
+		File file = new File(whereToSave, String.join("/", pathArray)) ;
+
+		File parent = file.getParentFile();
+
+		if (!parent.exists()) {
+			parent.mkdirs();
+		}
+
+		if(file.exists()){
+			file.delete();
+		}
+
+		try {
+
+			if(file.createNewFile()){
+				System.out.println("Start download: " + url);
+				FileUtils.copyURLToFile(new URL(url), file);
+				System.out.println("End download: " + url);
+			}
+
+		} catch (IOException e) {
+			System.out.println("file wasn't created: " + path);
+		}
+	}
+
 
 	public static void main( String[] args ) throws Exception {
 
 		System.setProperty("webdriver.gecko.driver", PATH_WEBDRIVER);
 		System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE,"/dev/null");
 
-		nextBranch(URL)
-				.parallelStream()
-				.forEach(System.out::println);
+		JFrame frame = new JFrame("InputDialog Example #1");
 
+		JFileChooser fileChooser = new JFileChooser();
+
+		fileChooser.setDialogTitle("Select folder to download images from AWS");
+
+		fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+		fileChooser.setAcceptAllFileFilterUsed(false);
+
+		String url = JOptionPane.showInputDialog(
+				frame,
+				"You can search at https://remotepixel.ca/projects/satellitesearch.html\n" +
+						"Example Sentinel: http://sentinel-s2-l1c.s3-website.eu-central-1.amazonaws.com/#tiles/21/L/UE/\n" +
+						"Example Landsat: https://landsatonaws.com/L8/227/070",
+				"What is the aws url that you want download?",
+				JOptionPane.PLAIN_MESSAGE
+		);
+
+		if (!Strings.isNullOrEmpty(url) && fileChooser.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) {
+
+			nextBranch(url)
+					.parallelStream()
+					.forEach(href -> createSaveFile(fileChooser.getSelectedFile(), href));
+
+		} else {
+			System.out.println("No Selection ");
+		}
+
+		System.exit(0);
 	}
 }
