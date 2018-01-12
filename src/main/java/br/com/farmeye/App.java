0251529ab1c62437c42f2ac2f7a693729f2e47bc
luiz <luizaugusto3b@gmail.com>
2018-01-12 21:30:10 -0200

    Write some thought for refactoring.
    Now THe algorithm shows messages more eficiently

--- src/main/java/br/com/farmeye/App.java
+++ src/main/java/br/com/farmeye/App.java
@@ -2,28 +2,146 @@ package br.com.farmeye;
 
 import com.machinepublishers.jbrowserdriver.JBrowserDriver;
 import com.machinepublishers.jbrowserdriver.Settings;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.validator.UrlValidator;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 
+import javax.swing.*;
+import javax.swing.text.DefaultCaret;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class App {
 
+	private static JTextArea textArea;
+
 	public static void main( String[] args ){
 
-		String urlDownload = args[0];
-		Double cloudPercentege = args.length < 2 ? 10 : Double.valueOf(args[1]);
-		String filename = args.length < 3 ? "downloads" : args[3];
+		JFrame frame = new JFrame("Log");
+
+		String urlDownload = setUrlDownload();
+		File folderToSave = chooseFolder();
+
+		textArea = new JTextArea();
+
+		textArea.setEditable(false);
+		textArea.setText("Download Scenes from " + urlDownload + "\n");
+		frame.getContentPane().add(new JScrollPane(textArea), BorderLayout.CENTER);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(new Dimension(800, 600));
+		frame.setVisible(true);
+
+		List<String> linksDownload = getLinksDownload(urlDownload);
+		List<String> errorLinks = new ArrayList<>();
+
+		linksDownload
+				.parallelStream()
+				.map(link -> {
+					try {
+						return new URL(link);
+					} catch (MalformedURLException e) {
+						throw new RuntimeException(e);
+					}
+				})
+				.forEach(url -> {
+					try {
+						download(folderToSave, url);
+					} catch (IOException e) {
+						errorLinks.add(url.toString());
+					}
+				});
+
+
+		errorLinks.removeAll(errorLinks.parallelStream().filter(url -> {
+			try {
+				download(folderToSave, new URL(url));
+				return true;
+			} catch (IOException e) {
+				textArea.append(url + "\n");
+				textArea.setCaretPosition(textArea.getDocument().getLength());
+				return false;
+			}
+
+		}).collect(Collectors.toList()));
+
+		textArea.append("\n");
+		textArea.append("\n");
+		textArea.append("Next Urls had problems to Download:\n");
+		textArea.setCaretPosition(textArea.getDocument().getLength());
+		errorLinks.parallelStream().forEach(System.out::println);
+
+		textArea.append("\n");
+		textArea.append("Finished");
+		textArea.setCaretPosition(textArea.getDocument().getLength());
+
+	}
+
+	private static void download(File folderToSave, URL url) throws IOException {
+		String filename = FilenameUtils.getName(url.getPath());
+		String folderPath = url.getPath().replace(filename, "");
+
+		File folder = new File(folderToSave, folderPath);
+
+		if(!folder.exists()){
+			folder.mkdirs();
+		}
+
+		File file = new File(folder, filename);
+		String intern = url.toString().intern();
+		synchronized(intern){
+			textArea.append("Downloading " +url+ "\n");
+			textArea.setCaretPosition(textArea.getDocument().getLength());
+		}
+
+		FileUtils.copyURLToFile(url, file, 5000, 5000);
+
+		synchronized(intern){
+			textArea.append("Finished " +url+ "\n");
+			textArea.setCaretPosition(textArea.getDocument().getLength());
+		}
+	}
+
+	private static String getHref(WebElement link) {
+		return link.getAttribute("href");
+	}
+
+
+	private static List<String> getUrls(List<WebElement> links, int urlLength){
+		if(isLeaf(links)){
+
+			return links.parallelStream()
+					.map(App::getHref)
+					.filter(link ->  canDownload(link))
+					.collect(Collectors.toList());
+
+		} else {
+
+			return links.parallelStream()
+					.filter(link ->  isBranchsLinks(link, urlLength))
+					.map(App::getHref)
+					.collect(Collectors.toList());
+
+		}
+	}
+
+	private static List<String> getLinksDownload(String urlDownload){
 
 		Settings settings = Settings
 				.builder()
-				.headless(false)
+				.quickRender(true)
+				.headless(true)
 				.javascript(true)
 				.cache(true)
+				.ajaxResourceTimeout(10000)
 				.connectTimeout(0)
 				.socketTimeout(0)
 				.build();
@@ -32,31 +150,36 @@ public class App {
 
 		driver.get(urlDownload);
 
-		String windowName = driver.getWindowHandle();
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.MINUTES);
 
-		driver.manage().timeouts().implicitlyWait(2, TimeUnit.MINUTES);
+		List<String> links = getUrls(driver.findElements(By.tagName("a")), urlDownload.length());
+		List<String> linksDownload = new ArrayList<>();
 
-		driver.findElements(By.tagName("a"))
-				.stream()
-				.filter(link ->  isBranchsLinks(link, urlDownload.length()))
-				.forEach(link -> {
-					System.out.println(getHref(link));
-					new Actions(driver)
-							.keyDown(Keys.CONTROL)
-							.click(link)
-							.keyUp(Keys.CONTROL)
-							.build()
-							.perform();
-					driver.switchTo().window(windowName);
-				});
+		while (!links.isEmpty()) {
+			String url = links.get(0);
+			textArea.append("Broswing at " +url+ "\n");
+			textArea.setCaretPosition(textArea.getDocument().getLength());
+			driver.navigate().to(url);
+			List<WebElement> linksElements =  driver.findElements(By.tagName("a"));
+			List<String> urls = getUrls(linksElements, url.length());
+			if(isLeaf(linksElements)){
+				linksDownload.addAll(urls);
+			} else {
+				links.addAll(urls);
+			}
+			links.remove(0);
+		}
 
 		driver.quit();
 
-
+		return linksDownload;
 	}
 
-	private static String getHref(WebElement link) {
-		return link.getAttribute("href");
+	private static boolean canDownload(String link) {
+		return !link.equals("preview.jp2") &&
+				(link.endsWith(".jp2") ||
+						link.equals("tileInfo.json") ||
+						link.equals("preview.jpg"));
 	}
 
 	private static boolean isBranchsLinks(WebElement element, Integer sizeUrl) {
@@ -64,6 +187,69 @@ public class App {
 
 		return link.length() >= sizeUrl &&
 				link.substring(sizeUrl).length() > 0;
+	}
+
+	private static Boolean isLeaf(List<WebElement> links){
+
+		return links
+				.parallelStream()
+				.map(App::getHref)
+				.anyMatch(link -> link.endsWith(".jp2"));
+
+	}
+
+	private static String setUrlDownload(){
+
+		String textDescription = "You can search at https://remotepixel.ca/projects/satellitesearch.html\n" +
+				"Example Sentinel: http://sentinel-s2-l1c.s3-website.eu-central-1.amazonaws.com/#tiles/21/L/UE/\n" +
+				//"Example Landsat: https://landsatonaws.com/L8/227/070";
+				"Download Landsat data still in development.";
+		String textQuestion = "What is the aws url that you want download?";
+
+
+		String url = JOptionPane.showInputDialog(
+				null,
+				textDescription,
+				textQuestion,
+				JOptionPane.PLAIN_MESSAGE
+		);
+
+		UrlValidator urlValidator = new UrlValidator();
+
+		if(urlValidator.isValid(url)){
+
+			return url;
+
+		} else {
+
+			JOptionPane.showMessageDialog(null, "Url format is wrong!");
+			System.exit(0);
+			return null;
+
+		}
+	}
+
+	private static File chooseFolder(){
+
+		JFileChooser fileChooser = new JFileChooser();
+
+		fileChooser.setDialogTitle("Select folder to download images from AWS");
+
+		fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+		fileChooser.setAcceptAllFileFilterUsed(false);
+
+		if(fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION){
+
+			return fileChooser.getSelectedFile();
+
+		} else {
+
+			JOptionPane.showMessageDialog(null, "Folder path was not selected!");
+			System.exit(0);
+			return null;
+		}
+
 	}
 
 }
